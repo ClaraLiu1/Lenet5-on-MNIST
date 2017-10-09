c601cff6f278ea30d2a0df19b483f479968a809b
Hazel <hazelsuko@gmail.com>
2017-10-09 10:40:42 +0800

    Modify output rate to per 100 steps

--- mnist_train.py
+++ mnist_train.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 import os
 import numpy as np
-
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
@@ -11,11 +10,12 @@ from IPython import embed
 # 加载mnist_inference.py中定义的常量和前向传播的函数
 import mnist_inference
 
-# 配置神经网络的参数
+# 每次读取样本数
 BATCH_SIZE = 100
 LEARNING_RATE_BASE = 0.01
 LEARNING_RATE_DECAY = 0.99
 REGULARAZTION_RATE = 0.0001
+# 总的训练次数
 TRAINING_STEPS = 30000
 MOVING_AVERAGE_DECAY = 0.99
 # 模型保存的路径和文件名
@@ -23,14 +23,13 @@ MODEL_SAVE_PATH = "model/"
 MODEL_NAME = "model.ckpt"
 
 def train(mnist):
-    # 定义输入输出placeholder
-    # 调整输入数据placeholder的格式，输入为一个四维矩阵
+    # 输入数据placeholder的格式，输入为一个四维矩阵
     x = tf.placeholder(tf.float32, [
         BATCH_SIZE,                             # 第一维表示一个batch中样例的个数
         mnist_inference.IMAGE_SIZE,             # 第二维和第三维表示图片的尺寸
         mnist_inference.IMAGE_SIZE,
         mnist_inference.NUM_CHANNELS],          # 第四维表示图片的深度，对于RBG格式的图片，深度为5
-                       name='x-input')
+        name='x-input')
     y_ = tf.placeholder(tf.float32, [None, mnist_inference.OUTPUT_NODE], name='y-input')
 
     regularizer = tf.contrib.layers.l2_regularizer(REGULARAZTION_RATE) #？？？
@@ -45,27 +44,27 @@ def train(mnist):
     cross_entropy_mean = tf.reduce_mean(cross_entropy)
     loss = cross_entropy_mean + tf.add_n(tf.get_collection('losses'))
     learning_rate = tf.train.exponential_decay(LEARNING_RATE_BASE, global_step, mnist.train.num_examples/BATCH_SIZE, LEARNING_RATE_DECAY)
+    # 使用反向传播进行优化，最小化损失函数
     train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
     with tf.control_dependencies([train_step, variable_averages_op]):
         train_op = tf.no_op(name='train')
 
-    # 初始化Tensorflow持久化类
+    # train.Saver是自动加断点、保存函数
     saver = tf.train.Saver()
     with tf.Session() as sess:
+        # 初始化全局变量
         tf.global_variables_initializer().run()
-        # 验证和测试的过程将会有一个独立的程序来完成
         for i in range(TRAINING_STEPS):
             xs, ys = mnist.train.next_batch(BATCH_SIZE)
-            embed()
-            #类似地将输入的训练数据格式调整为一个四维矩阵，并将这个调整后的数据传入sess.run过程
+            # 断点调试：embed()
+            # 将输入的训练数据格式调整为一个四维矩阵
             reshaped_xs = np.reshape(xs, (BATCH_SIZE, mnist_inference.IMAGE_SIZE, mnist_inference.IMAGE_SIZE, mnist_inference.NUM_CHANNELS))
             _, loss_value, step = sess.run([train_op, loss, global_step], feed_dict={x: reshaped_xs, y_: ys})
             #每1000轮保存一次模型。
-            if i%1000 == 0:
+            if i%100 == 0:
                 # 输出当前的训练情况。这里只输出了模型在当前训练batch上的损失函数大小。通过损失函数的大小可以大概了解训练的情况。
-                # 在验证数据集上的正确率信息会有一个单独的程序来生成。
                 print("After %d training step(s), loss on training batch is %f." % (step, loss_value))
-                # 保存当前的模型。注意这里隔出了global_step参数，这样可以让每个被保存模型的文件名末尾加上训练的轮数，比如“model.ckpt-1000”表示训练1000轮后得到的模型
+                # 保存当前的模型。规定了保存的格式，global_step参数，这样可以让每个被保存模型的文件名末尾加上训练的轮数，比如“model.ckpt-1000”表示训练1000轮后得到的模型
                 saver.save(sess, os.path.join(MODEL_SAVE_PATH, MODEL_NAME), global_step=global_step)
 
 
